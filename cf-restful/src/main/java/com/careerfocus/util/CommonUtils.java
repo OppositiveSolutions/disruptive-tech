fa873e2a9434e73cd05b97aeb70700f7991041a2
sarath <sarath@cinchplc.com>
2018-01-13 17:40:23 +0530

    announcement image file name

--- cf-restful/src/main/java/com/careerfocus/util/CommonUtils.java
+++ cf-restful/src/main/java/com/careerfocus/util/CommonUtils.java
@@ -4,9 +4,14 @@ import com.careerfocus.util.response.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
 
 public class CommonUtils {
 
@@ -22,6 +27,24 @@ public class CommonUtils {
         } catch (IOException e) {
             log.error("Error", e);
         }
+    }
+
+    public static String getImageTypeFromByteArray(byte[] data) {
+        String imageType = "jpeg";
+        try {
+            ImageInputStream iis = ImageIO
+                    .createImageInputStream(new ByteArrayInputStream(data));
+
+            Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
+
+            if (readers.hasNext()) {
+                ImageReader read = readers.next();
+                imageType = read.getFormatName().toLowerCase();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return imageType;
     }
 
 }
