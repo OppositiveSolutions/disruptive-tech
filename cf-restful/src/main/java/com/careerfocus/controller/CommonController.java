939c7346e187e1f37413981a52ed32d1cffff5d9
Alex <alex@greencirclehealth.com>
2018-02-16 00:00:37 +0530

    Alex:Coaching_types

--- cf-restful/src/main/java/com/careerfocus/controller/CommonController.java
+++ cf-restful/src/main/java/com/careerfocus/controller/CommonController.java
@@ -9,21 +9,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/common")
 public class CommonController {
 
-    @Autowired
-    CommonService commonService;
+	@Autowired
+	CommonService commonService;
 
-    @RequestMapping(value = "/log4j/{level}", method = RequestMethod.GET)
-    public Response setLogLevel(@PathVariable("level") int level) throws Exception {
-        return Response.ok(commonService.setLogLevel(level)).build();
-    }
+	@RequestMapping(value = "/log4j/{level}", method = RequestMethod.GET)
+	public Response setLogLevel(@PathVariable("level") int level) throws Exception {
+		return Response.ok(commonService.setLogLevel(level)).build();
+	}
 
-    @RequestMapping(value = "/states", method = RequestMethod.GET)
-    public Response getStates() throws Exception {
-        return Response.ok(commonService.getStates()).build();
-    }
+	@RequestMapping(value = "/states", method = RequestMethod.GET)
+	public Response getStates() throws Exception {
+		return Response.ok(commonService.getStates()).build();
+	}
 
-    @RequestMapping(value = "/email/is-valid", method = RequestMethod.GET)
-    public Response checkEmailExists(@RequestParam("emailId") String emailId) throws Exception {
-        return commonService.checkEmailExists(emailId);
-    }
+	@RequestMapping(value = "/email/is-valid", method = RequestMethod.GET)
+	public Response checkEmailExists(@RequestParam("emailId") String emailId) throws Exception {
+		return commonService.checkEmailExists(emailId);
+	}
+
+	@RequestMapping(value = "/coachingtypes", method = RequestMethod.GET)
+	public Response getCoachingTypes() throws Exception {
+		return Response.ok(commonService.getCoachingTypes()).build();
+	}
 }
