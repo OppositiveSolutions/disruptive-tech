666d9c3ca28f74eedbd20bb41c32492a616e69c0
Alex <alex@greencirclehealth.com>
2018-03-21 00:54:05 +0530

    Alex:Bundle_image

---
+++ cf-restful/src/main/java/com/careerfocus/repository/BundleImageRepository.java
@@ -0,0 +1,8 @@
+package com.careerfocus.repository;
+
+import com.careerfocus.entity.BundleImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BundleImageRepository extends JpaRepository<BundleImage, Integer> {
+
+}
